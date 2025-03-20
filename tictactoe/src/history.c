e3d05fcb5e8528880e5d8b8ea0048e3bb9b69f5e
ZleiVV <chayanzygas@gmail.com>
2025-03-20 10:40:04 +0100

    Delete tictactoe directory

--- tictactoe/src/history.c
+++
@@ -1,7 +0,0 @@
-#include "history.h"
-#include <stdio.h>
-
-// Sauvegarde d'une partie dans un fichier ou en mémoire
-void save_game_history(GameState *game_state) {
-    // Sauvegarde des résultats de la partie dans un fichier ou base de données
-}
